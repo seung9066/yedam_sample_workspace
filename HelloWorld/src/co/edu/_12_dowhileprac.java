546cb46bc08581d1bdc82f943cdbbfec41fd48e8
admin <admin@YD-18>
2022-07-04 16:55:42 +0900

    a

---
+++ HelloWorld/src/co/edu/_12_dowhileprac.java
@@ -0,0 +1,26 @@
+package co.edu;
+
+import java.util.Scanner;
+
+public class _12_dowhileprac {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		boolean istrue = true;
+		int sum = 0;
+		
+		do {
+			int num = scan.nextInt();
+			sum += num;
+			if (sum > 100) {
+				System.out.println("반복문 종료");
+				istrue = false;
+			} else {
+				System.out.printf("현재 누적값 : %d\n", sum);
+			}
+		} while (istrue);
+		System.out.println("sum의 누적값 : " + sum);
+		System.out.println("end.");
+	}
+
+}
