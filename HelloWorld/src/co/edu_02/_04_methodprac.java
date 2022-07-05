1ac624b823c20287d48b3e977734efcb1f41eb26
admin <admin@YD-18>
2022-07-05 17:33:42 +0900

    a

--- HelloWorld/src/co/edu_02/_04_methodprac.java
+++ HelloWorld/src/co/edu_02/_04_methodprac.java
@@ -5,18 +5,25 @@ public class _04_methodprac {
 
 	public static void gugudan(int a) {
 		for (int i = 1; i <= 9; i++) {
-			System.out.println(a+ "단 : " + a * i);
+			System.out.printf("%d * %d = %d\n", a, i, (a * i));
 		}
+		System.out.println();
 	}
 
 	public static void gugu(int a, int b) {
-		System.out.println("결과 : " + a * b);
+		System.out.printf("%d * %d = %d\n", a, b, (a * b));
+		System.out.println();
 	}
-	
+
 	public static void main(String[] args) {
 		
+		System.out.println("gugudan");
 		gugudan(3);
-		gugu(3, 7);
-		
+		System.out.println("gugu");
+		gugu(2, 7);
+		System.out.println("응용");
+		for (int i=2; i<=5; i++) // method를 응용한 2~5단 출력
+			gugudan(i);
+
 	}
 }
