1e76a2638defe94cc6db174d03453e371c331f60
admin <admin@YD-18>
2022-07-07 09:02:07 +0900

    a

--- HelloWorld/src/test/exam06.java
+++ HelloWorld/src/test/exam06.java
@@ -3,33 +3,28 @@ package test;
 import java.util.Scanner;
 
 public class exam06 {
-    /*
-     * 문제 5: 배열.
-     * Scanner 클래스를 사용하여 사용자가 입력한 값을 다음의 배열에서 찾아서 값이 있을 경우에는 해당 값의 저장위치를 값이 없을 경우에는
-     * 찾는 값이 없습니다는 메세지를 보여주도록 기능을 작성하세요.
-     */
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String[] fruits = {"사과", "딸기", "감", "수박", "포도", "복숭아"};
+	/*
+	 * 문제 5: 배열. Scanner 클래스를 사용하여 사용자가 입력한 값을 다음의 배열에서 찾아서 값이 있을 경우에는 해당 값의 저장위치를
+	 * 값이 없을 경우에는 찾는 값이 없습니다는 메세지를 보여주도록 기능을 작성하세요.
+	 */
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String[] fruits = { "사과", "딸기", "감", "수박", "포도", "복숭아" };
 
-        boolean run = true;
-        String a = scn.nextLine();
+		boolean run = true;
+		String a = scn.nextLine();
 
-        while (run) {
-            for (int i = 0; i < fruits.length; i++) {
-                if (a.equals(fruits[i])) {
-                    System.out.println(i);
-                }
+		while (run) {
+			for (int i = 0; i < fruits.length; i++) {
+				if (a.equals(fruits[i])) {
+					System.out.println(i);
+				} else {
+					if (i == fruits.length -1){
+						System.out.println("찾는 값이 없습니다.");
+					}
+				}
 
-                while (run) {
-                    if (a != fruits[i]) {
-                        System.out.println("찾는 값이 없습니다.");
-                        run = false;
-                    }
-                }
-            }
-
-        }
-    }
-
+			}
+		}
+	}
 }
