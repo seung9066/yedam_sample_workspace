5e6c58a0c536b459dd3e3e038a5b752f97828ed4
admin <admin@YD-18>
2022-07-08 17:51:08 +0900

    a

--- HelloWorld/src/co/edu_06_interfaces/employee/Employeearraylist.java
+++ HelloWorld/src/co/edu_06_interfaces/employee/Employeearraylist.java
@@ -1,5 +1,41 @@
 package co.edu_06_interfaces.employee;
 
-public class Employeearraylist {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * 컬렉션 기반으로 기능 구현
+ */
+public class Employeearraylist implements Employeelist {
+	
+	private static Employeearraylist instance = new Employeearraylist();
+	private Employeearraylist() {}
+	public static Employeearraylist getInstance() {
+		return instance;
+	}
+	
+	ArrayList<Employee> list = new ArrayList<Employee>();
+	int empNum; // 필요없음 add로 계속 넣으면 됨
+	Scanner scan = new Scanner(System.in);
+	
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void input() {
+		
+	}
+
+	@Override
+	public Employee search(int empId) {
+		return null;
+	}
+
+	@Override
+	public void printList() {
+		
+	}
 
 }
