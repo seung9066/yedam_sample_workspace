d74a6c56f1dd63e28a447942fed1a28fc2a1a784
admin <admin@YD-18>
2022-07-08 10:37:52 +0900

    a

---
+++ HelloWorld/src/co/edu_05_inheritance/_01_main.java
@@ -0,0 +1,16 @@
+package co.edu_05_inheritance;
+
+public class _01_main {
+	
+	public static void main(String[] args) {
+		
+		_01_dmbcellphone phone = new _01_dmbcellphone();
+		phone.powerOn();
+		phone.bell();
+		phone.sendvoice();
+		phone.receiveVoice();
+		phone.powerOff();
+		
+	}
+
+}
