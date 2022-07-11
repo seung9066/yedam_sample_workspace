a98bc98c8e6fa0386d53b5db47fdec60c510af2f
admin <admin@YD-18>
2022-07-11 10:10:41 +0900

    a

--- HelloWorld/src/co/edu_07_exception/_04_InputMismatch.java
+++ HelloWorld/src/co/edu_07_exception/_04_InputMismatch.java
@@ -1,5 +1,6 @@
 package co.edu_07_exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class _04_InputMismatch {
@@ -7,7 +8,11 @@ public class _04_InputMismatch {
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("숫자 입력");
-		int num = scan.nextInt();
+		try {
+			int num = scan.nextInt(); // int 타입인데 문자열을 쳤을 때
+		} catch (InputMismatchException e) { // 잘못된 타입의 변수가 들어올 때
+			System.out.println("정수를 입력하시오");
+		}
 		
 		System.out.println("프로그램 종료");
 		
