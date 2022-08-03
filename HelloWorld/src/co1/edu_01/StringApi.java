8e9af1e1d9f96fdeac1913196c9e2f79f4bcbd3a
admin <admin@YD-18>
2022-08-03 13:08:34 +0900

    a

--- HelloWorld/src/co1/edu_01/StringApi.java
+++ HelloWorld/src/co1/edu_01/StringApi.java
@@ -2,6 +2,22 @@ package co1.edu_01;
 
 public class StringApi {
 	public static void main(String[] args) {
+		
+		// 100번지
+		String a = "aaa";
+		// 101번지
+		a = a + "bbb";
+		// 두 메모리를 차지함
+		
+		// 메모리 관리 효율에 좋음
+		StringBuilder sb = new StringBuilder();
+		
+		// 100번지
+		sb.append("aaa");
+		// 100번지
+		sb.append("bb");
+		
+		
 		// == 주소값 비교
 		// .equals 데이터 비교
 	}
