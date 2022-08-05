fd77e1babe3a771bbcbc18d6743422e7f3e6186d
admin <admin@YD-18>
2022-08-05 09:18:48 +0900

    a

---
+++ JDBC_/src/com/yedam02/member/MemberDTO.java
@@ -0,0 +1,8 @@
+package com.yedam02.member;
+
+public class MemberDTO {
+	private String memberId; // member_id
+	private String memberPw; // member_pw
+	private String memberName; // member_name
+	private String memberBelong; // member_belong
+}
