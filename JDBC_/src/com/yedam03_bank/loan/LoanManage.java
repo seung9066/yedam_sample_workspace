a348aab7a677f5153ef7aa3e2b09c3fba2431ade
admin <admin@YD-18>
2022-08-08 10:36:39 +0900

    a

--- JDBC_/src/com/yedam03_bank/loan/LoanManage.java
+++ JDBC_/src/com/yedam03_bank/loan/LoanManage.java
@@ -1,5 +1,8 @@
 package com.yedam03_bank.loan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.yedam03_bank.common.DAO;
 
 public class LoanManage extends DAO {
@@ -20,12 +23,13 @@ public class LoanManage extends DAO {
 		int result = 0;
 		try {
 			conn();
-			String sql = "insert into loan values(?,?,?,?)";
+			String sql = "insert into loan values(?,?,?,?,?)";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, loan.getLoanId());
 			pstmt.setString(2, loan.getMemberId());
 			pstmt.setDate(3, loan.getLoanDate());
 			pstmt.setString(4, loan.getState());
+			pstmt.setInt(5, loan.getLoanMoney());
 			
 			result = pstmt.executeUpdate();
 		} catch (Exception e) {
@@ -53,4 +57,53 @@ public class LoanManage extends DAO {
 		}
 		return result;
 	}
+	
+	public int updateMoney(Loan loan) {
+		int result = 0;
+		try {
+			conn();
+			String sql = "update loan set loan_money = loan_money - ? where loan_id= ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, loan.getLoanMoney());
+			pstmt.setString(2, loan.getLoanId());
+			
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		return result;
+	}
+
+	// join
+	public List<Loan> getLoanInfo(String memberId) {
+		List<Loan> list = new ArrayList<Loan>();
+		Loan loan = null;
+		try {
+			conn();
+			String sql = "select b.member_name member_name, l.loan_id loan_id, l.loan_money loan_money, l.loan_date loan_date\r\n"
+					+ "from bankmember b join loan l\r\n"
+					+ "on b.member_id = l.member_id\r\n"
+					+ "where b.member_id = '?'";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				loan = new Loan();
+				loan.setMemberName(rs.getString("member_name"));
+				loan.setLoanId(rs.getString("loan_id"));
+				loan.setLoanMoney(rs.getInt("loan_money"));
+				loan.setLoanDate(rs.getDate("loan_date"));
+				list.add(loan);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();// TODO: handle exception
+		} finally {
+			disconn();
+		}
+		return list;
+	}
+	
 }
