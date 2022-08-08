e82a2dfb20fe5745674249877d6ef8ffa974d9d6
admin <admin@YD-18>
2022-08-08 09:45:16 +0900

    a

--- JDBC_/src/com/yedam03_bank/loan/LoanManage.java
+++ JDBC_/src/com/yedam03_bank/loan/LoanManage.java
@@ -16,6 +16,41 @@ public class LoanManage extends DAO {
 		return lm;
 	}
 	
-	public 
+	public int insertLoan(Loan loan) {
+		int result = 0;
+		try {
+			conn();
+			String sql = "insert into loan values(?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, loan.getLoanId());
+			pstmt.setString(2, loan.getMemberId());
+			pstmt.setDate(3, loan.getLoanDate());
+			pstmt.setString(4, loan.getState());
+			
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		return result;
+	}
 	
+	public int updateLoan(Loan loan) {
+		int result = 0;
+		try {
+			conn();
+			String sql = "update loan set state = ? where loan_id= ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, loan.getState());
+			pstmt.setString(2, loan.getLoanId());
+			
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		return result;
+	}
 }
