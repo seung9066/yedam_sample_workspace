6cac154f844be2394ec43522f9f73cdef6b51d90
admin <admin@YD-18>
2022-08-25 16:02:06 +0900

    a

--- web/src/main/webapp/javascript/login.js
+++ web/src/main/webapp/javascript/login.js
@@ -1,16 +1,35 @@
+function login() {
+    var logid = document.getElementById("logid").value;
+    var logpw = document.getElementById("logpw").value;
+
+    if (logid == "") {
+        alert("아이디를 입력하세요!")
+    } else if (logpw == "") {
+        alert("일치하지 않는 비밀번호 입니다!")
+    } else {
+        location.href="login copy.html";
+    }
+}
+
+function account_page() {
+    document.getElementById("e").style.display = "none";
+    document.getElementById("d").style.display = "block";
+}
+
 function check() {
     var id = document.getElementById("id").value;
     var pw = document.getElementById("pw").value;
     var pw1 = document.getElementById("pw1").value;
     if (id == "") {
         alert("아이디를 입력하세요!")
-    } else if (pw == "") {
+    } else if (pw == "" && pw1 == "") {
         alert("비밀번호를 입력하세요!")
-    } else if (pw != pw1) {
+    } else if (pw != pw1 || pw1 != "" && pw == "") {
         alert("비밀번호가 일치하지 않습니다!")
     } else {
         document.getElementById("a").style.display = "none";
         document.getElementById("b").style.display = "block";
+        document.getElementById("name").focus();
     }
 }
 
@@ -28,9 +47,20 @@ function menu() {
 
 }
 
-function enter_key() {
+function checkid() {
+    alert("사용가능 합니다!")
+}
+
+function enter_key(next, next1) {
     if (window.event.keyCode == 13) {
-        document.getElementById("next").click();
+        document.getElementById(next).click();
+        document.getElementById(next1).focus();
+    }
+}
+
+function enter_key1(next) {
+    if (window.event.keyCode == 13) {
+        document.getElementById(next).focus();
     }
 }
 
