48a03941e5dfb29a194358246bf8c1bb343f3689
admin <admin@YD-18>
2022-08-26 17:48:24 +0900

    a

--- web/src/main/webapp/javascript/calender.js
+++ web/src/main/webapp/javascript/calender.js
@@ -1,4 +1,4 @@
-document.write('<br>-------------------foreach.js에서 가져온 글들-------------------<br>')
+document.write('<br>-------------------calender.js에서 가져온 글들-------------------<br>')
 
 document.write('<br><div style="border: 1px solid black; padding: 10px"><p><span><</span>달력<span>></span></p>')
 let cal = '<table border=1>';
@@ -20,34 +20,34 @@ cal += '</table>';
 document.write(cal);
 document.write('</div>')
 
-document.write('<br><div style="border: 1px solid black; padding: 10px"><p><span><</span>function 달력<span>></span></p>')
+document.write('<br><div id="calcu" style="border: 1px solid black; padding: 10px"><p><span><</span>function 달력<span>></span></p>')
 
 function getEmptyCnt(month) {
     switch (month) {
         case 1:
             return 6;
         case 2:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 3:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 4:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 5:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 6:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 7:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 8:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 9:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 10:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 11:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
         case 12:
-            return ((getEmptyCnt(month-1) + getLastDay(month-1)) % 7);
+            return ((getEmptyCnt(month - 1) + getLastDay(month - 1)) % 7);
     }
 }
 
@@ -81,10 +81,11 @@ function getLastDay(month) {
 
 }
 
-let cal1 = '<table border=1 style="text-align: center;">';
-cal1 += '<tr><th style="color: red;">일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th>';
+let cal1 = '<table border=1 style="text-align: center; width: 500px; height: 300px">';
+cal1 += '<tr><th style="color: red;">일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th style="color: blue;">토</th></tr>';
 cal1 += '<tr>'
 let month = 12;
+document.write(month + '월의 달력')
 let empty1 = getEmptyCnt(month);
 let last1 = getLastDay(month);
 for (let f = 0; f < empty1; f++) {
@@ -93,6 +94,8 @@ for (let f = 0; f < empty1; f++) {
 for (let day = 1; day <= last1; day++) {
     if ((empty1 + day) % 7 == 1) {
         cal1 += `<td style="color: red;">${day}</td>`;
+    } else if ((empty1 + day) % 7 == 0){
+        cal1 += `<td style="color: blue;">${day}</td>`;
     } else {
         cal1 += `<td>${day}</td>`;
     }
@@ -100,9 +103,9 @@ for (let day = 1; day <= last1; day++) {
         cal1 += '<tr></tr>';
     }
 }
-cal1 += '</tr></tr>';
+cal1 += '</tr>';
 cal1 += '</table>';
 document.write(cal1);
 document.write('</div>')
 
-document.write('<br>-------------------foreach.js에서 가져온 글들-------------------<br>')
+document.write('<br>-------------------calender.js에서 가져온 글들-------------------<br>')
