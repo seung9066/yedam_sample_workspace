10bf33a11d5cff750bc6e797049510349872d9c4
admin <admin@YD-18>
2022-08-31 17:48:23 +0900

    a

--- web/src/main/webapp/practice/dom_mainpulate2.js
+++ web/src/main/webapp/practice/dom_mainpulate2.js
@@ -19,10 +19,49 @@ function liston() {
     let tel1 = document.getElementById('mtel').value;
     tdtel.innerText = tel1;
 
-    tr.appendChild(tdid);
-    tr.appendChild(tdpw);
-    tr.appendChild(tdtel);
-
+    if (!id1 || !name1 || !tel1) {
+        alert('값을 입력하세요')
+        return; // 함수종료
+    } else {
+        tr.appendChild(tdid);
+        tr.appendChild(tdpw);
+        tr.appendChild(tdtel);
+    }
 
     document.getElementById('list').appendChild(tr);
+
+    // 초기화
+    mid.value = '';
+    mname.value = '';
+    mtel.value = '';
+    mid.focus();
+};
+
+let btn2 = document.querySelector('#on1');
+btn2.onclick = function () {
+    let tr = document.createElement('tr');
+    let id1 = document.getElementById('mid').value;
+    let name1 = document.getElementById('mname').value;
+    let tel1 = document.getElementById('mtel').value;
+
+    const mValues = [id1, name1, tel1];
+
+    if (!id1 || !name1 || !tel1) {
+        alert('값을 입력하세요')
+        return; // 함수종료
+    } else {
+        mValues.forEach(val => {
+            let td = document.createElement('td');
+            let txt = document.createTextNode(val);
+            td.appendChild(txt);
+            tr.appendChild(td);
+        });
+        document.getElementById('list').appendChild(tr);
+    }
+
+    // 초기화
+    mid.value = '';
+    mname.value = '';
+    mtel.value = '';
+    mid.focus();
 };
