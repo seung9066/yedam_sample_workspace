9a35910dc2560bbbf631c1882f6c00a21a76731a
admin <admin@YD-18>
2022-09-05 10:49:18 +0900

    a

--- web/src/main/java/jdbc/MyCalendarServlet.java
+++ web/src/main/java/jdbc/MyCalendarServlet.java
@@ -7,26 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/MyCalendarServlet")
+@WebServlet("/myCalendar")
 public class MyCalendarServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-   
+ 
     public MyCalendarServlet() {
         super();
-   
+     
     }
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
 		doGet(request, response);
 	}
 
