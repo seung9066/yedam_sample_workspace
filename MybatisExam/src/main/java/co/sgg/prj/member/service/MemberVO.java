c1ca5d5a363a22f113e6b9d17c83d1a0f2ec4623
choeseunghyeon <seoung9066@naver.com>
2022-09-17 18:58:41 +0900

    a

--- MybatisExam/src/main/java/co/sgg/prj/member/service/MemberVO.java
+++ MybatisExam/src/main/java/co/sgg/prj/member/service/MemberVO.java
@@ -11,5 +11,36 @@ public class MemberVO {
     private String  memberName; 
     private String  memberTel;
     private String  memberAuthor;
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPassWord() {
+		return memberPassWord;
+	}
+	public void setMemberPassWord(String memberPassWord) {
+		this.memberPassWord = memberPassWord;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberTel() {
+		return memberTel;
+	}
+	public void setMemberTel(String memberTel) {
+		this.memberTel = memberTel;
+	}
+	public String getMemberAuthor() {
+		return memberAuthor;
+	}
+	public void setMemberAuthor(String memberAuthor) {
+		this.memberAuthor = memberAuthor;
+	}
 	
+    
 }
